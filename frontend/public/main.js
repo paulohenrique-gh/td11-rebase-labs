cf6d43abf2b5de27ddef918187103f657ffd2964
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-03-10 12:22:53 -0300

    Refatora main.js e informa filtragem no cabeçalho da listagem

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,33 +1,23 @@
 const url = 'http://localhost:3000/tests'
-const examList = document.querySelector('.exam-list');
-
 const searchForm = document.querySelector('.search-form');
+
+const examList = document.querySelector('.exam-list');
 const examDetails = document.createElement('div')
 
-let currentContent = examList;
-let newContent = examDetails;
+function examHTML(exam) {
+  let tests = '';
 
-function loadExamList() {
-  fetch(url).
-  then((response) => response.json()).
-  then((data) => {
-  
-      data.forEach((exam) => {
-        const li = document.createElement('li');
-        let tests = '';
-  
-        exam.tests.forEach((test) => {
-          tests += `
-            <li class="test-listing--item">
-              <span>${test.test_type}</span>
-              <span>${test.test_type_limits}</span>
-              <span>${test.test_type_results}</span>
-            </li>
-          `
-        });
-  
-        li.innerHTML = `
-          <div class="exam-section">
+  exam.tests.forEach((test) => {
+      tests += `
+        <li class="test-listing--item">
+          <span>${test.test_type}</span>
+          <span>${test.test_type_limits}</span>
+          <span>${test.test_type_results}</span>
+        </li>
+      `
+  });
+
+  return `<div class="exam-section">
             <div class="general-data">
               <div><strong>Token:</strong> ${exam.exam_result_token}</div>
               <div><strong>Data do resultado:</strong> ${exam.exam_result_date}</div>
@@ -57,7 +47,7 @@ function loadExamList() {
                 <div><strong>Email:</strong> ${exam.doctor.doctor_email}</div>
               </div>
             </div>
-  
+
             <div class="test-data">
               <div class="test-data--header">
                 <div class="title">Testes</div>
@@ -73,8 +63,18 @@ function loadExamList() {
                 </ul>
               </div>
             </div>
-          </div>
-        ` 
+          </div>`
+}
+
+function loadExamList() {
+  fetch(url).
+  then((response) => response.json()).
+  then((data) => {
+  
+      data.forEach((exam) => {
+        const li = document.createElement('li');
+  
+        li.innerHTML = examHTML(exam);
         examList.appendChild(li);
       });
     }).
@@ -87,9 +87,10 @@ searchForm.onsubmit = function(event) {
   event.preventDefault();
 
   let token = event.target[0].value;
-  let filter = document.querySelector('.filter');
+  let filter = document.querySelector('.filter-value');
 
   if (!token) {
+    filter.textContent = 'Todos';
     examDetails.replaceWith(examList);
     return;
   }
@@ -115,57 +116,13 @@ searchForm.onsubmit = function(event) {
         `
       });
 
-      examDetails.innerHTML = `
-        <div class="exam-section">
-          <div class="general-data">
-            <div><strong>Token:</strong> ${exam.exam_result_token}</div>
-            <div><strong>Data do resultado:</strong> ${exam.exam_result_date}</div>
-          </div>
-          <div class="patient-data">
-            <div class="patient-data--header">
-              <div class="title">Paciente</div>
-              <div class="name">${exam.patient.patient_name}</div>
-            </div>
-            <div class="patient-data--info">
-              <div><strong>CPF:</strong> ${exam.patient.patient_cpf}</div>
-              <div><strong>E-mail:</strong> ${exam.patient.patient_email}</div>
-              <div><strong>Data de nascimento:</strong> ${exam.patient.patient_birthdate}</div>
-              <div><strong>Endereço:</strong> ${exam.patient.patient_address}</div>
-              <div><strong>Cidade:</strong> ${exam.patient.patient_city}</div>
-              <div><strong>Estado:</strong> ${exam.patient.patient_state}</div>
-            </div>
-          </div>
-          <div class="doctor-data">
-            <div class="doctor-data--header">
-              <div class="title">Médico</div>
-              <div class="name">${exam.doctor.doctor_name}</div>
-            </div>
-            <div class="doctor-data--info">
-              <div><strong>CRM:</strong> ${exam.doctor.doctor_crm}</div>
-              <div><strong>Estado CRM:</strong> ${exam.doctor.doctor_crm_state}</div>
-              <div><strong>Email:</strong> ${exam.doctor.doctor_email}</div>
-            </div>
-          </div>
+      examDetails.innerHTML = examHTML(exam);
+      examDetails.innerHTML += `
+        <button class="back-button">
+          <ion-icon name="arrow-back-circle-outline" class="back-icon"></ion-icon>
+        </button>
+      `
 
-          <div class="test-data">
-            <div class="test-data--header">
-              <div class="title">Testes</div>
-            </div>
-            <div class="test-listing">
-              <div class="test-listing--headers">
-                <span>Tipo</span>
-                <span>Limites</span>
-                <span>Resultado</span>
-              </div>
-              <ul class="test-listing--info">
-                ${tests}
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <button class="back-button"><ion-icon name="arrow-back-circle-outline" class="back-icon"></ion-icon></button>
-      `;
       filter.innerHTML = `${token}`;
 
       let backButton = document.querySelector('.back-button')
@@ -175,7 +132,7 @@ searchForm.onsubmit = function(event) {
       });
     });
 
-  currentContent.replaceWith(newContent);
+  examList.replaceWith(examDetails);
 };
 
 
