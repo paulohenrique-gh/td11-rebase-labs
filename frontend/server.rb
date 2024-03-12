3c7fe4e18da4489a55b31fddd1ed30b3c31b3fff
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-03-11 23:41:18 -0300

    Refatora server para incluir rotas que fazem requisições para o backend
    - Inclui gems para testes

--- frontend/server.rb
+++ frontend/server.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'rack/handler/puma'
+require 'faraday'
 
 set :port, 4000
+
+BACKEND_API_URL = 'http://labs-backend:3000/tests'.freeze
 
 get '/' do
   content_type 'text/html'
@@ -9,6 +12,17 @@ get '/' do
   File.open('index.html')
 end
 
-get '/tests' do
-  Faraday.get('http://localhost:3000/tests')
+get '/load-tests' do
+  content_type :json
+  response = Faraday.get(BACKEND_API_URL)
+
+  response.body
+end
+
+if ENV['RACK_ENV'] == 'development' || ENV['RACK_ENV'] == 'production'
+  Rack::Handler::Puma.run(
+    Sinatra::Application,
+    Port: 4000,
+    Host: '0.0.0.0'
+  )
 end
---
+++ frontend/spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'rack/test'
+require 'sinatra'
+require_relative '../server'
+
+ENV['RACK_ENV'] = 'test'
+
+def app
+  Sinatra::Application
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
