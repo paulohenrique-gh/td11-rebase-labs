5a7f86489f3aff3a728ec6d6fa3d7611c55b651f
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-03-13 16:02:06 -0300

    Corrige testes de request do frontend

--- frontend/spec/requests/import_exams_spec.rb
+++ frontend/spec/requests/import_exams_spec.rb
@@ -6,14 +6,14 @@ describe '/import' do
     fake_response = double('Faraday::Response',
                            status: 200,
                            success?: true,
-                           body: { message: 'Data imported successfully.' }.to_json)
-    allow(Faraday).to receive(:get).and_return(fake_response)
+                           body: { message: 'Processing file.' }.to_json)
+    allow_any_instance_of(Faraday::Connection).to receive(:post).and_return(fake_response)
 
     post '/import', file: Rack::Test::UploadedFile.new(test_csv, 'text/csv')
 
     expect(last_response.status).to eq 200
     json_response = JSON.parse(last_response.body)
-    expect(json_response['message']).to eq 'Data imported successfully.'
+    expect(json_response['message']).to eq 'Processing file.'
   end
 
   it 'returns error when file format is invalid' do
@@ -29,7 +29,7 @@ describe '/import' do
   it '500 if backend request is not successfull' do
     fake_csv = 'spec/support/test.csv'
     fake_response = double('Faraday::Response', status: 500, success?: false)
-    allow(Faraday).to receive(:get).and_return(fake_response)
+    allow_any_instance_of(Faraday::Connection).to receive(:post).and_return(fake_response)
 
     post '/import', file: Rack::Test::UploadedFile.new(fake_csv, 'text/csv')
 
